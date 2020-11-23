69559bcdf8a5df2c4c0e837e2b7f189a2686fad3
Kerri <gant.kerri@gmail.com>
2020-11-23 03:12:49 -0500

    User api routes created

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-const { requireAuth } = require('../../utils/auth.js');
-const { restoreUser } = require('../../utils/auth.js');
+router.use('/session', sessionRouter);
 
-
+router.use('/users', usersRouter);
 
 module.exports = router;
