1ccb2bf202ebe64bb330dea02596634ea4df393e
Kerri <gant.kerri@gmail.com>
2020-11-23 03:01:08 -0500

    Auth Middleware Tested

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth.js');
+const { restoreUser } = require('../../utils/auth.js');
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+
 
 module.exports = router;
