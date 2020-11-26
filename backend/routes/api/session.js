1a08103d6fb4933abf3bb106258185764af97cb8
Kerri <gant.kerri@gmail.com>
2020-11-26 01:33:04 -0500

    think i fixed sign in

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -48,7 +48,7 @@ router.post(
 router.delete(
     '/',
     (_req, res) => {
-        res.clearCookie('XSRF-TOKEN');
+        res.clearCookie('token');
         return res.json({ message: 'success' });
     }
 );
