78520ac5bef99a482ea257f1c4e8fdeee9da9360
Kerri <gant.kerri@gmail.com>
2020-11-26 00:57:21 -0500

    working on sign

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -29,7 +29,7 @@ const validateSignup = [
 ];
 
 router.post(
-    '',
+    '/',
     validateSignup,
     asyncHandler(async (req, res) => {
         const { email, password, username } = req.body;
